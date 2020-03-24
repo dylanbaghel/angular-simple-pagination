dac81c7bd9c8acb31771e5e2821a288e20d4f3e8
Dylan Baghel <baghelabhi39@gmail.com>
2020-03-24 23:30:52 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives-pages';
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the beach',
+      url: 'assets/beach.jpeg'
+    },
+    {
+      title: 'At the mountain',
+      url: 'assets/mountain.jpeg'
+    },
+    {
+      title: 'Swimming Pool',
+      url: 'assets/pool.jpeg'
+    },
+    {
+      title: 'Volcano',
+      url: 'assets/volcano.jpeg'
+    },
+    {
+      title: 'At the beach',
+      url: 'assets/beach.jpeg'
+    },
+    {
+      title: 'At the mountain',
+      url: 'assets/mountain.jpeg'
+    },
+    {
+      title: 'Swimming Pool',
+      url: 'assets/pool.jpeg'
+    },
+    {
+      title: 'Volcano',
+      url: 'assets/volcano.jpeg'
+    },
+    {
+      title: 'At the beach',
+      url: 'assets/beach.jpeg'
+    },
+    {
+      title: 'At the mountain',
+      url: 'assets/mountain.jpeg'
+    },
+    {
+      title: 'Swimming Pool',
+      url: 'assets/pool.jpeg'
+    },
+    {
+      title: 'Volcano',
+      url: 'assets/volcano.jpeg'
+    },
+    {
+      title: 'At the beach',
+      url: 'assets/beach.jpeg'
+    },
+    {
+      title: 'At the mountain',
+      url: 'assets/mountain.jpeg'
+    },
+    {
+      title: 'Swimming Pool',
+      url: 'assets/pool.jpeg'
+    },
+    {
+      title: 'Volcano',
+      url: 'assets/volcano.jpeg'
+    }
+  ];
+
+  onPageChange(page: number) {
+    this.currentPage = page;
+  }
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
